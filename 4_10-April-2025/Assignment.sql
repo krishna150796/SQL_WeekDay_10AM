66a50944dce304014d3e6346d9b1f65f77b79777
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2025-04-10 11:06:25 +0530

    Date 10 April 2025
    Joins
    Assignment

---
+++ 4_10-April-2025/Assignment.sql
@@ -0,0 +1,28 @@
+tbl_CategoryMaster
+	[Category_Id]
+	[Category_Name]
+	[Modified_Date]
+	[Created_Date]
+	[IsDeleted]
+
+tbl_SubcategoryMaster
+	[Subcategory_Id]
+	[Category_Id]
+	[Subcategory_Name]
+	[SubCategory_Price]
+	[SubCategory_Quantity]
+	[Created_Date]
+	[Modified_Date]
+	[IsDeleted]
+
+tbl_OrderMaster
+	[Order_Id]
+	[Subcategory_Id]
+	[Order_Date]
+	[Order_Qty]
+	[Order_Amt]
+
+
+
+
+
